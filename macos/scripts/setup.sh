0efb48bfb3774763bbc4c03cc4dd97338d7139c0
rainx0r <angelos.h@hotmail.com>
2024-07-18 23:28:49 +0100

    Improve control flow in macOS setup

--- macos/scripts/setup.sh
+++ macos/scripts/setup.sh
@@ -1,11 +1,23 @@
 SKIP_INSTALL=false
+SKIP_LINKS=false
+SKIP_BREW=false
+SKIP_MISC=false
 
 # Parse command line arguments
 for arg in "$@"
 do
-    if [ "$arg" = "--no-install" ]
+    if [ "$arg" = "--no-system" ]
     then
         SKIP_INSTALL=true
+    elif [ "$arg" = "--no-links" ]
+    then
+        SKIP_LINKS=true
+    elif [ "$arg" = "--no-brew" ]
+    then
+        SKIP_BREW=true
+    elif [ "$arg" = "--no-misc" ]
+    then
+        SKIP_MISC=true
     fi
 done
 
@@ -18,26 +30,44 @@ then
     curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
     brew install stow
 else
-    echo "Skipping Oh My Zsh & brew installs"
+    echo "Skipping system installs"
 fi
 
 # Symlink files
-if [ ! -d "macos" ] && [ ! -d "shared" ]; then
-    echo "Please call this script from the root of the 'dotfiles' directory." >&2
-    exit 1
+if [ "$SKIP_LINKS" = false ]
+then
+    if [ ! -d "macos" ] && [ ! -d "shared" ]; then
+        echo "Please call this script from the root of the 'dotfiles' directory." >&2
+        exit 1
+    fi
+    stow -t $HOME --adopt macos
+    stow -t $HOME --adopt shared
+else
+    echo "Skipping symlinks"
 fi
-stow -t $HOME --adopt macos
-stow -t $HOME --adopt shared
 
 # Install software
-# - Software via brew
-brew bundle --file ~/.config/Brewfile
+if [ "$SKIP_BREW" = false ]
+then
+    brew bundle --file ~/.config/Brewfile
+else
+    echo "Skipping brew installs"
+fi
 
 # Set up VSCode
-cp -r vscode/* ~/Library/Application\ Support/Code/User/
+if [ "$SKIP_MISC" = false ]
+then
+    echo "- Setting up VSCode..."
+    cp -r vscode/* ~/Library/Application\ Support/Code/User/
+    echo "- - VSCode set up!"
 
-# Set Safari CSS
-defaults write com.apple.safari UserStyleSheetEnabled 1\
- && defaults write com.apple.safari UserStyleSheetLocationURLString "~/.config/safari.css"\
- && defaults write com.apple.safari WebKitUserStyleSheetEnabledPreferenceKey 1\
- && defaults write com.apple.safari WebKitUserStyleSheetLocationPreferenceKey "~/.config/safari.css"
+    # Set Safari CSS
+    echo "- Setting up Safari CSS..."
+    defaults write com.apple.safari UserStyleSheetEnabled 1\
+    && defaults write com.apple.safari UserStyleSheetLocationURLString "~/.config/safari.css"\
+    && defaults write com.apple.safari WebKitUserStyleSheetEnabledPreferenceKey 1\
+    && defaults write com.apple.safari WebKitUserStyleSheetLocationPreferenceKey "~/.config/safari.css"
+    echo "- - Safari CSS set up!"
+else
+    echo "Skipping misc configurations"
+fi
