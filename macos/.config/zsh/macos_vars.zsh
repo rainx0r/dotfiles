4011729d962330fb2b69599d5d33bb10e2b0d4b1
rainx0r <angelos.h@hotmail.com>
2024-12-07 02:03:32 +0000

    Update Zed Keybinds + MacOS vars for ghcup

--- macos/.config/zsh/macos_vars.zsh
+++ macos/.config/zsh/macos_vars.zsh
@@ -2,3 +2,4 @@ export PATH="$HOME/.local/bin:/opt/homebrew/bin:$PATH"
 export DISPLAY=:0
 export MODULAR_HOME="/Users/$USER/.modular"
 export PATH="/Users/$USER/.modular/pkg/packages.modular.com_mojo/bin:$PATH"
+export PATH="/Users/$USER/.ghcup/bin:$PATH"
