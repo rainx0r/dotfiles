f86786d33ead9e3da624af51cb75413137f02ebe
rainx0r <angelos.h@hotmail.com>
2025-01-13 22:40:24 +0000

    Move widgets.zsh to linux + mac separately for `find` differences

--- linux/.config/zsh/linux_vars.zsh
+++ linux/.config/zsh/linux_vars.zsh
@@ -1,3 +1,3 @@
 export PATH="$PATH:$HOME/.local/bin"
-export LD_LIBRARY_PATH="/usr/local/lib:/usr/local/cuda-12.5"
+export LD_LIBRARY_PATH="/home/rain/.local/lib/arch-mojo:/usr/local/lib:/usr/local/cuda-12.5"
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=/opt/cuda
